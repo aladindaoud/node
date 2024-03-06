f5f80604559d82806cf7027c569a0e64057da659
aladindaoud <83234713+aladindaoud@users.noreply.github.com>
2024-03-06 11:14:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,155 @@
-module.exports = app => {
-  const tutorials = require("../controllers/tutorial.controller.js");
 const express = require("express");
+
 const app = express();
-var router = require("express").Router();
+
 require("dotenv").config();
+
 app.use(express.json());
+
 const connectDB = require("./connectMongo");
+
 connectDB();
-const BookModel = require("./models/tutorial.model");
+
+const BookModel = require("./models/book.model");
 const redis = require('./redis')
 
-var router = require("express").Router();
+const deleteKeys = async (pattern) => {
+  const keys = await redis.keys(`${pattern}::*`)
+  console.log(keys)
+  if (keys.length > 0) {
+    redis.del(keys)
+  }
+}
 
-// Create a new Tutorial
-router.post("/", tutorials.create);
+app.get("/api/v1/books", async (req, res) => {
+  const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
+  let page = +req.query?.page;
 
-// Retrieve all Tutorials
-router.get("/", tutorials.findAll);
+  if (!page || page <= 0) page = 1;
 
-// Retrieve all published Tutorials
-router.get("/published", tutorials.findAllPublished);
+  const skip = (page - 1) * + limit;
 
-// Retrieve a single Tutorial with id
-router.get("/:id", tutorials.findOne);
+  const query = {};
 
-// Update a Tutorial with id
-router.put("/:id", tutorials.update);
+  if (keyword) query.name = { $regex: keyword, $options: "i" };
 
-// Delete a Tutorial with id
-router.delete("/:id", tutorials.delete);
+  const key = `Book::${JSON.stringify({query, page, limit, orderBy, sortBy})}`
+  let response = null
+  try {
+    const cache = await redis.get(key)
+    if (cache) {
+      response = JSON.parse(cache)
+    } else {
+      const data = await BookModel.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort({ [orderBy]: sortBy });
+      const totalItems = await BookModel.countDocuments(query);
 
-// Create a new Tutorial
-router.delete("/", tutorials.deleteAll);
+      response = {
+        msg: "Ok",
+        data,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit),
+        limit: +limit,
+        currentPage: page,
+      }
 
-app.use("/api/tutorials", router);
+      redis.setex(key, 600, JSON.stringify(response))
+    }
+    
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
 
+app.get("/api/v1/books/:id", async (req, res) => {
+  try {
+    const data = await BookModel.findById(req.params.id);
 
-};
+    if (data) {
+      return res.status(200).json({
+        msg: "Ok",
+        data,
+      });
+    }
+
+    return res.status(404).json({
+      msg: "Not Found",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
+
+app.post("/api/v1/books", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const book = new BookModel({
+      name,
+      author,
+      price,
+      description,
+    });
+    const data = await book.save();
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
+
+app.put("/api/v1/books/:id", async (req, res) => {
+  try {
+    const { name, author, price, description } = req.body;
+    const { id } = req.params;
+
+    const data = await BookModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        author,
+        price,
+        description,
+      },
+      { new: true }
+    );
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+      data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
+
+app.delete("/api/v1/books/:id", async (req, res) => {
+  try {
+    await BookModel.findByIdAndDelete(req.params.id);
+    deleteKeys('Book')
+    return res.status(200).json({
+      msg: "Ok",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: error.message,
+    });
+  }
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
